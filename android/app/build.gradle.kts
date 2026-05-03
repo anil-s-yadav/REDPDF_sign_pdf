a80b7105c19b00fd6e3510f5cbbc3530c4a480c8
anil_s_yadav <paytmking15@gmail.com>
2026-05-03 19:51:38 +0530

    signin

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,19 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+// Load keystore properties
+val keystorePropertiesFile = rootProject("key.properties")
+val keystoreProperties = Properties().apply {
+    if (keystorePropertiesFile.exists()) {
+        load(FileInputStream(keystorePropertiesFile))
+    }
 }
 
 android {
@@ -15,39 +26,32 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions { jvmTarget = JavaVersion.VERSION_17.toString() }
-
-    defaultConfig {
-        // TODO: Specify your own unique Application ID
-        // (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.lagendarysoftware.sign_pdf_scan_pdf"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    signingConfigs {
+        create("release") {
+            keyAlias     = keystoreProperties["keyAlias"]     as String
+            keyPassword  = keystoreProperties["keyPassword"]  as String
+            storeFile    = file(keystoreProperties["storeFile"] as String)
+            storePassword= keystoreProperties["storePassword"] as String
         }
     }
-}
 
-flutter { source = "../.." }
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+    defaultConfig {
+        applicationId = "com.lagendarysoftware.sign_pdf_scan_pdf"
+        minSdk        = flutter.minSdkVersion
+        targetSdk     = flutter.targetSdkVersion
+        versionCode   = flutter.versionCode
+        versionName   = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled   = false
+            isShrinkResources = false
         }
     }
 }
